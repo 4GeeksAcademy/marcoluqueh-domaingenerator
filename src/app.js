ee4d0da2dbbc3a347ef08217bd17b9b1d986eb79
Marco Luque <101642586+marcoluqueh@users.noreply.github.com>
2024-03-25 19:48:34 +0000

    Ciao prettier

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuseWho = who[Math.floor(Math.random() * who.length)];
+  let excuseAction = action[Math.floor(Math.random() * action.length)];
+  let excuseWhat = what[Math.floor(Math.random() * what.length)];
+  let excuseWhen = when[Math.floor(Math.random() * when.length)];
+
+  let generateExcuse = excuseWho + +excuseAction + excuseWhat + excuseWhen;
+  document.querySelector("#excuse").innerHTML = generateExcuse;
 };
